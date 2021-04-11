65d57129570bf5428415ee6ecbf9e065e364725d
sou <sido0903@gmail.com>
2021-04-11 15:19:10 +0900

    SQL更新しました

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,23 +1,16 @@
-CREATE TABLE buy_history (
-    order_num int(11) AUTO_INCREMENT,
+CREATE TABLE orders (
+    order_id int(11) AUTO_INCREMENT,
     user_id int(11),
     created datetime,
-    PRIMARY KEY (order_num)
+    PRIMARY KEY (order_id)
 );
 
-CREATE TABLE buy_amount (
-    Processing_number int(11) AUTO_INCREMENT,
-    order_num int(11),
-    item_num int(11),
+CREATE TABLE order_items (
+    id int(11) AUTO_INCREMENT,
+    order_id int(11),
+    item_id int(11),
     amount int(11),
-    PRIMARY KEY (Processing_number)
-);
-
-CREATE TABLE item_details (
-    Processing_number int(11) AUTO_INCREMENT,
-    order_num int(11),
-    item_num int(11),
     item_name VARCHAR(100),
-    item_value int(11),
-    PRIMARY KEY (Processing_number)
+    item_price int(11)
+    PRIMARY KEY (id)
 );
