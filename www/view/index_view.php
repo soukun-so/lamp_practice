2cd7c85177a85a66e547c081e90cb853e69eddb1
sou <sido0903@gmail.com>
2021-04-18 10:08:34 +0900

    ランキング機能を追加しました

--- www/view/index_view.php
+++ www/view/index_view.php
@@ -42,6 +42,46 @@
       <?php } ?>
       </div>
     </div>
+
+    <h1>商品ランキング</h1>
+
+    <?php if(count($rankings) > 0){ ?>
+      <table class="table table-bordered">
+        <thead class="thead-light">
+          <tr>
+            <th>順位</th>
+            <th>画像</th>
+            <th>商品名</th>
+            <th>値段</th>
+            <th>購入</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach($rankings as $ranking){ ?>
+          <tr>
+            <td><p><?php print $rank += 1;?></p></td>
+            <td><img src="<?php print (h(IMAGE_PATH . $ranking['image']));?>" style="width: 200px"></td>
+            <td><?php print(h($ranking['name'])); ?></td>
+            <td><?php print (h(number_format($ranking['price']))); ?>円</td>
+            <td>
+              <?php if($ranking['stock'] > 0){ ?>
+                <form action="index_add_cart.php" method="post">
+                  <input type="submit" value="カートに追加" class="btn btn-primary btn-block">
+                  <input type="hidden" name="item_id" value="<?php print (h($ranking['item_id'])); ?>">
+                  <input type="hidden" name="token" value="<?php print $token; ?>">
+                </form>
+                <?php } else { ?>
+                  <p class="text-danger">現在売り切れです。</p>
+                <?php } ?>
+            </td>
+          </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    <?php } else { ?>
+      <p>カートに商品はありません。</p>
+    <?php } ?> 
+
   </div>
   
 </body>
